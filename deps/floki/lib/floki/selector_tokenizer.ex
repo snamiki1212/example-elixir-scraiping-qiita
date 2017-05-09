0fa2e1acec6faa3789e93413ac84b176f587085c
namiky <jazzzzzzzzzzzz12@gmail.com>
2017-05-09 22:54:04 +0900

    Add files via upload

---
+++ deps/floki/lib/floki/selector_tokenizer.ex
@@ -0,0 +1,13 @@
+defmodule Floki.SelectorTokenizer do
+  @moduledoc false
+
+  # It decodes a given selector and returns the tokens that represents it.
+  # Check the rules in "src/floki_selector_lexer.xrl"
+  def tokenize(selector) do
+    char_list = selector |> String.strip |> String.to_char_list
+
+    {:ok, token_list, _} = :floki_selector_lexer.string(char_list)
+
+    token_list
+  end
+end
